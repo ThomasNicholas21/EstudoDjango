f1bbf7b7c44588f7430d96e7eb6b6dcf12ce2ff3
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-08 20:53:05 -0300

    Ajustando views

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_view(request):
-    return render(request, 'blog.html')
+    return render(request, 'blog/index.html')
 
 def road_map(request):
-    return render(request, 'roadmap.html')
+    return render(request, 'blog/roadmap.html')
