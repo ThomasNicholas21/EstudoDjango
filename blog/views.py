d3f540a2866700c85ca756da61587f0a4f14efb5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-14 22:19:54 -0300

    Ädicinando variavel title para blog e roadmap

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def blog_view(request):
     context = {
-        'variavel': 'Isso é uma variável utilizando a função render'
+        'variavel': 'Isso é uma variável utilizando a função render',
+        'title': 'Blog'
     }
     return render(request, 'blog/index.html', context)
 
 def road_map(request):
     context = {
-        'variavel': 'Isso é uma variável utilizando a função render'
+        'variavel': 'Isso é uma variável utilizando a função render',
+        'title': 'RoadMap'
     }
     return render(request, 'blog/roadmap.html', context)
