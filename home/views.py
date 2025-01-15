e0582063935deae7db9b1bc0d1b557dc6df5fdfc
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-14 22:19:23 -0300

    adicionando variável title

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     context = {
-        'variavel': 'Isso é uma variável utilizando a função render'
+        'variavel': 'Isso é uma variável utilizando a função render',
+        'title': 'Home'
     }
     return render(request, 'home/index.html', context)
