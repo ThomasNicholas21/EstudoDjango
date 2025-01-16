c2fb9e09325d0975370b77c831d89bc92321ae2d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-16 20:33:33 -0300

    configurando URL dinâmica

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_view, name='blog'),
+    path('post/<id>', views.post_view, name='post'),
     path('roadmap/', views.road_map, name='roadmap')
 ]
