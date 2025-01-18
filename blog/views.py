550cc9c07ba739104b189773574dcb6e2ae85ea2
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-17 22:56:35 -0300

    tipando funções

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from blog.data import posts
-
+from typing import Any
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request: HttpRequest) -> HttpResponse:
     context = {
         'variavel': 'artigos',
         'title': 'Blog',
@@ -11,9 +12,9 @@ def blog_view(request):
     }
     return render(request, 'blog/index.html', context)
 
-def post_view(request, post_id):
+def post_view(request: HttpRequest, post_id: int) -> HttpResponse:
     print('post', id)
-    found_id = None
+    found_id: dict[str, Any] | None = None
 
     for post in posts:
         if post['id'] == post_id:
@@ -28,7 +29,7 @@ def post_view(request, post_id):
 
     return render(request, 'blog/post.html', context)
 
-def road_map(request):
+def road_map(request: HttpRequest) -> HttpResponse:
     context = {
         'variavel': 'Isso é uma variável utilizando a função render',
         'title': 'RoadMap'
